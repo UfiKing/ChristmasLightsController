b6ec660a5711e1e4f5b9d40356ba7cc4a342f757
Ufi <jure@hodak.net>
2025-12-11 15:09:24 +0100

    rewrote lights controlling as a class

--- src/lightsClass.cpp
+++ src/lightsClass.cpp
@@ -1,44 +1,36 @@
 #include "lightsClass.h"
 #include <FastLED.h>
+
 #define LED_PIN 18
-#define NUM_LEDS 399
 
-class LightsHandler{
-  CRGB leds[NUM_LEDS];
-  uint8_t brightness = 10;
-  uint8_t hue = 0;
+void LightsHandler::lightsOff(){
+  LightsHandler::state = false;
+  for(uint16_t i = 0; i < LightsHandler::numLeds ; i++){
+    LightsHandler::leds[i] = CRGB(0,0,0);
+  }
+  FastLED.show();
+}
 
-  public: 
-    LightsHandler(){
-      FastLED.addLeds<WS2812B, LED_PIN>(leds, NUM_LEDS);
-      FastLED.setBrightness(brightness);
-    }
+void LightsHandler::lightsOn(){
+  LightsHandler::state = true;
+  for(uint16_t i = 0; i < LightsHandler::numLeds; i++){
+    LightsHandler::leds[i] = CRGB(255,255,255);
+  }
+  FastLED.show();
+}
 
-    void lightsOff(){
-      for(uint16_t i = 0; i<NUM_LEDS; i++){
-        leds[i] = CRGB(0,0,0);
-      }
-      FastLED.show();
-    }
-
-    void lightsOn(){
-      for(uint16_t i = 0; i<NUM_LEDS; i++){
-        leds[i] = CRGB(255,255,255);
-      }
-      FastLED.show();
-    }
-
-    void rainbowEffect1(){
-      for(uint16_t i = NUM_LEDS - 1; i > 0; i--){
-        leds[i] = leds[i - 1];
-      }
-      leds[0] = CHSV(hue,255,255);
-      hue++;
-      
-      FastLED.show();
-    }
+void LightsHandler::rainbowEffect1(){
+  for(uint16_t i = LightsHandler::numLeds - 1; i > 0; i--){
+    LightsHandler::leds[i] = LightsHandler::leds[i - 1];
+  }
+  leds[0] = CHSV(LightsHandler::hue,255,255);
+  LightsHandler::hue++;
+   
+  FastLED.show();
+}
 
 
-};
+
+
 
 
--- src/lightsClass.h
+++ src/lightsClass.h
@@ -1,7 +1,27 @@
+#include <FastLED.h>
 #ifndef LIGHTS_CLASS_H
 #define LIGHTS_CLASS_H
+#define LED_PIN 18
 
+class LightsHandler{
+  uint8_t brightness = 10;
+  uint8_t hue = 0;  
+  const uint16_t numLeds = 399;
+  CRGB leds[399];
+  public: 
 
-class LightsHandler;
+  bool state = false;
+  LightsHandler(){
+    FastLED.addLeds<WS2812B, LED_PIN>(leds, numLeds);
+    FastLED.setBrightness(brightness);
+  }
+  
+  void lightsOff();
+
+  void lightsOn();
+
+  void rainbowEffect1();
+
+};
 
 #endif
